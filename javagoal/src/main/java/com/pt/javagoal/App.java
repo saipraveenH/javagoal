5a5052e814b6342e1c935af91251889db7d7c47e
KARTHIK <KARTHIK@DESKTOP-H0M3LM4>
2020-04-21 07:54:07 -0700

    inital commit.

---
+++ javagoal/src/main/java/com/pt/javagoal/App.java
@@ -0,0 +1,13 @@
+package com.pt.javagoal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
