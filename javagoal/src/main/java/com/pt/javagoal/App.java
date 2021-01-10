bf5eabadf0dd29e906dfe38d7d57b9e71b8ff17e
praveen <hemadripraveen@gmail.com>
2021-01-10 19:04:07 +0530

    added service, controller for product apis

--- javagoal/src/main/java/com/pt/javagoal/App.java
+++ javagoal/src/main/java/com/pt/javagoal/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World! 2nd time" );
+        System.out.println( "Hello World!from naga e" );
     }
 }
